56b8025b9c07de5ef9bba15ba25ef4fef7b46ae9
suraj <surajmore2143@gmail.com>
2023-01-22 20:53:27 +0530

    Added to Diplay the Table on Web brouser Task feature

---
+++ WebApplication3/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+
+namespace WebApplication3.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+        MVCDBEntities db = new MVCDBEntities();
+
+        public ActionResult Index()
+        {
+            List<Student> lst = db.Students.ToList();
+            return View(lst);
+        }
+
+	}
+}
